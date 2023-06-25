77eb557ad092221b697ec44b445fd57bcdb7a84b
TOMATOZERO\chima <chima05.2015@gmail.com>
2023-06-25 15:48:05 +0300

    feat(game logic): implement TransformRotate script

--- Assets/Scripts/TasksScripts/TransformRotate.cs
+++ Assets/Scripts/TasksScripts/TransformRotate.cs
@@ -1,17 +1,38 @@
-using System;
 using UnityEngine;
 
 public class TransformRotate : MonoBehaviour
 {
+    [SerializeField] private Transform _transform;
+    [Space]
+    [SerializeField] private float _rotationSpeed = 45;
+    [SerializeField] private Vector3 _rotation;
+    [Space]
     [SerializeField] private Color _color;
-    [SerializeField] private bool _isQuaternion; 
+    [SerializeField] private bool _isQuaternion;
+
+    private Vector3 _currentEulerAngeles; 
     
     private void Start()
     {
         var renderers = gameObject.GetComponentsInChildren<MeshRenderer>();
         foreach (var mesh in renderers) mesh.material.color = _color;
     }
-    
-    
-    
+
+    private void FixedUpdate()
+    {
+        _rotation.Normalize();
+        
+        if (_isQuaternion)
+        {
+            var target = Quaternion.Euler(_rotation * (_rotationSpeed * Time.fixedDeltaTime));
+            transform.rotation *= target;
+        }
+        else
+        {
+            _currentEulerAngeles += _rotation * (Time.fixedDeltaTime * _rotationSpeed);
+            _transform.eulerAngles = _currentEulerAngeles;
+            // _transform.Rotate(_currentEulerAngeles);
+        }
+        
+    }
 }
