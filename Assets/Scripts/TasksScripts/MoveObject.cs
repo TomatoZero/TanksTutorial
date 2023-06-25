876261b00c17690b4d1d9f3561de9347c16fc483
TOMATOZERO\chima <chima05.2015@gmail.com>
2023-06-25 18:00:11 +0300

    feat(game logic): implement MoveObject script

---
+++ Assets/Scripts/TasksScripts/MoveObject.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace TankTutorial.Scripts.TaskScripts
+{
+    public class MoveObject : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private Vector3 _moveDirection;
+        [SerializeField] private float _speed;
+
+        private void FixedUpdate()
+        {
+            _rigidbody.MovePosition(_rigidbody.position + _moveDirection.normalized * (_speed * Time.fixedDeltaTime));
+        }
+    }   
+}
