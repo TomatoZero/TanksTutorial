08e413b5d306546d4a81d1c6420c049d70f55993
TOMATOZERO\chima <chima05.2015@gmail.com>
2023-07-03 12:51:05 +0300

    feat(game logic): implement counting passing through

--- Assets/Scripts/TasksScripts/BoxScript.cs
+++ Assets/Scripts/TasksScripts/BoxScript.cs
@@ -6,21 +6,10 @@ public class BoxScript : MonoBehaviour
     [SerializeField] private TMP_Text _countPassThrough;
 
     private int _count;
-    
-    private void OnCollisionEnter(Collision other)
-    {
-        Physics.IgnoreLayerCollision(gameObject.layer, other.gameObject.layer);
-    }
 
-    private void OnCollisionExit(Collision other)
+    private void OnTriggerExit(Collider other)
     {
-        Physics.IgnoreLayerCollision(gameObject.layer, other.gameObject.layer);
         _count++;
         _countPassThrough.text = $"Pass Through {_count}";
-    }
-
-    private void OnCollisionStay(Collision other)
-    {
-        Debug.Log("3");
     }
 }
