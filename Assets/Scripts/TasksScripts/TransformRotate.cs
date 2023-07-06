058fba72e9377622b368635420ae2d0a3389b201
TOMATOZERO\chima <chima05.2015@gmail.com>
2023-07-06 19:44:52 +0300

    feat(game logic): implement diffetent way to rotate object

--- Assets/Scripts/TasksScripts/TransformRotate.cs
+++ Assets/Scripts/TasksScripts/TransformRotate.cs
@@ -9,9 +9,10 @@ namespace TankTutorial.Scripts.TaskScripts
         [SerializeField] private Vector3 _rotation;
         [Space] [SerializeField] private Color _color;
         [SerializeField] private bool _isQuaternion;
-        
+
 
         private Vector3 _currentEulerAngeles;
+        private Vector3 _rotationAngles;
 
         private void Start()
         {
@@ -22,17 +23,32 @@ namespace TankTutorial.Scripts.TaskScripts
         private void FixedUpdate()
         {
             _rotation.Normalize();
+            var rotationAmount = _rotationSpeed * 6f * Time.fixedDeltaTime;
             
             if (_isQuaternion)
             {
-                // var target = Quaternion.Euler(_rotation * (_rotationSpeed * 6f * Time.fixedDeltaTime));
+                // var target = Quaternion.AngleAxis(rotationAmount, _rotation);
+                // or
+                // var target = Quaternion.Euler(_rotation * rotationAmount);
+                
                 // transform.rotation *= target;
-                _transform.Rotate(_rotation, _rotationSpeed * 6f * Time.fixedDeltaTime);
+
+                _transform.Rotate(_rotation, rotationAmount);
+                
+                //Not permanent
+                // var startRotation = transform.rotation;
+                // var targetRotation = Quaternion.LookRotation(_rotation, Vector3.up);
+                // Quaternion newRotation = Quaternion.RotateTowards(startRotation, targetRotation, rotationAmount);
+                // transform.rotation = newRotation;
             }
             else
             {
-                _currentEulerAngeles += _rotation * (Time.fixedDeltaTime * _rotationSpeed * 6f);
+                _currentEulerAngeles += _rotation * rotationAmount;
                 _transform.eulerAngles = _currentEulerAngeles;
+                
+                // _rotationAngles += _rotation * rotationAmount;
+                // Quaternion rotationQuaternion = Quaternion.Euler(_rotationAngles);
+                // transform.rotation = rotationQuaternion;
             }
         }
     }
