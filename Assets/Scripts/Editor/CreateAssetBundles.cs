d0303218cb7637895a6fec7a13aa9f05d01ced75
TOMATOZERO\chima <chima05.2015@gmail.com>
2023-07-11 22:32:47 +0300

    feat(game logic): create particles folder and paste particles

---
+++ Assets/Scripts/Editor/CreateAssetBundles.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using UnityEditor;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    private static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
+            BuildAssetBundleOptions.None,
+            BuildTarget.StandaloneWindows);
+    }
+}
