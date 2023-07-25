a47c10102773d24303b5e9928201efb70261c25d
TOMATOZERO\chima <chima05.2015@gmail.com>
2023-07-25 13:54:36 +0300

    feat(game logic): implement GameData and CompanyData

--- Assets/Scripts/ScriptableObject/PlayMarket/CompanyData.cs
+++ Assets/Scripts/ScriptableObject/PlayMarket/CompanyData.cs
@@ -5,6 +5,23 @@ namespace TankTutorial.Scripts.ScriptableObject.PlayMarket
     [CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObject/PlayMarket/CompanyData", order = 0)]
     public class CompanyData : UnityEngine.ScriptableObject
     {
-        
+        [SerializeField] private string _name;
+        [SerializeField] private Sprite _logo;
+        [SerializeField] private string _email;
+        [SerializeField] private string _website;
+        [SerializeField] private string _address;
+        [SerializeField] private string _urlPrivacyPolicy;
+
+        public string Name => _name;
+
+        public Sprite Logo => _logo;
+
+        public string Email => _email;
+
+        public string Website => _website;
+
+        public string Address => _address;
+
+        public string URLPrivacyPolicy => _urlPrivacyPolicy;
     }
 }
--- Assets/Scripts/ScriptableObject/PlayMarket/GameData.cs
+++ Assets/Scripts/ScriptableObject/PlayMarket/GameData.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace TankTutorial.Scripts.ScriptableObject.PlayMarket
@@ -5,6 +6,14 @@ namespace TankTutorial.Scripts.ScriptableObject.PlayMarket
     [CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObject/PlayMarket/GameData", order = 0)]
     public class GameData : UnityEngine.ScriptableObject
     {
-        
+        [SerializeField] private CompanyData _company;
+        [SerializeField] private string _name;
+        [SerializeField] private Sprite _ico;
+        [Space]
+        [SerializeField] private List<Sprite> _gameExamples;
+        [SerializeField] private float _price;
+        [Space]
+        [SerializeField, Range(0, 5)] private float _rate;
+        [SerializeField] private List<int> _rates;
     }
 }
