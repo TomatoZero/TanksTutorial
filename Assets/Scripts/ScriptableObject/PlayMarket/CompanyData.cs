389d9715abe9e8b93b74302ad428dccf7d966e88
TOMATOZERO\chima <chima05.2015@gmail.com>
2023-07-25 13:01:12 +0300

    feat(game logic): create GameData scriptable object and CompanyData scriptable object

---
+++ Assets/Scripts/ScriptableObject/PlayMarket/CompanyData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace TankTutorial.Scripts.ScriptableObject.PlayMarket
+{
+    [CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObject/PlayMarket/CompanyData", order = 0)]
+    public class CompanyData : UnityEngine.ScriptableObject
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ScriptableObject/PlayMarket/GameData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace TankTutorial.Scripts.ScriptableObject.PlayMarket
+{
+    [CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObject/PlayMarket/GameData", order = 0)]
+    public class GameData : UnityEngine.ScriptableObject
+    {
+        
+    }
+}
