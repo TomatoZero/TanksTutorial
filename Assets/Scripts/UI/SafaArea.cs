1c18998861f7fb1c84ec77f59e1f68440bd39211
TOMATOZERO\chima <chima05.2015@gmail.com>
2023-08-04 12:09:55 +0300

    fix(ui logic): fix safe area

--- Assets/Scripts/UI/SafaArea.cs
+++ Assets/Scripts/UI/SafaArea.cs
@@ -1,3 +1,5 @@
+using System;
+using UnityEditor.DeviceSimulation;
 using UnityEngine;
 
 public class SafaArea : MonoBehaviour
@@ -6,19 +8,36 @@ public class SafaArea : MonoBehaviour
     [SerializeField] private RectTransform _panelTransform;
 
     private Rect _currentSafaArea = new Rect();
+    private Rect _canvasPixelRect = new Rect();
     private ScreenOrientation _currentOrientation = ScreenOrientation.AutoRotation;
+    private bool _isPanelTransformNull;
 
     private void Start()
     {
+        _isPanelTransformNull = _panelTransform == null;
         _currentOrientation = Screen.orientation;
         _currentSafaArea = Screen.safeArea;
+        _canvasPixelRect = _canvas.pixelRect;
         
         ApplySafaArea();
     }
 
+    private void FixedUpdate()
+    {
+        if ((_currentOrientation != Screen.orientation) || (_currentSafaArea != Screen.safeArea) || _canvasPixelRect != _canvas.pixelRect)
+        {
+            Debug.Log($"Before {_panelTransform.anchorMin} {_panelTransform.anchorMax}");
+            ApplySafaArea();
+            Debug.Log($"After {_panelTransform.anchorMin} {_panelTransform.anchorMax}");
+            Debug.Log("Screen.safeArea" + Screen.safeArea + " " + Screen.safeArea.position);
+            Debug.Log($"_canvas.pixelRect {_canvas.pixelRect}");
+            
+        }
+    }
+
     private void ApplySafaArea()
     {
-        if(_panelTransform == null) return;
+        if(_isPanelTransformNull) return;
 
         var safaArea = Screen.safeArea;
         var pixelRect = _canvas.pixelRect;
@@ -28,6 +47,31 @@ public class SafaArea : MonoBehaviour
 
         _currentOrientation = Screen.orientation;
         _currentSafaArea = Screen.safeArea;
+        _canvasPixelRect = pixelRect;
+    }
+
+    private void ApplySafeAreaOrign()
+    {
+        if(_isPanelTransformNull) return;
+
+        var safaArea = Screen.safeArea;
+
+        var anchorMin = safaArea.position;
+        var anchorMax = safaArea.position + safaArea.size;
+
+        var pixelRect = _canvas.pixelRect;
+        anchorMin.x /= pixelRect.width;
+        anchorMin.y /= pixelRect.height;
+
+        anchorMax.x /= pixelRect.width;
+        anchorMax.y /= pixelRect.height;
+
+        _panelTransform.anchorMin = anchorMin;
+        _panelTransform.anchorMax = anchorMax;
+        
+        _currentOrientation = Screen.orientation;
+        _currentSafaArea = Screen.safeArea;
+        _canvasPixelRect = pixelRect;
     }
 
     private Vector2 NormalizeSafeAreaPositionFollowingResolution(Vector2 anchor, Rect pixelRect)
@@ -37,4 +81,5 @@ public class SafaArea : MonoBehaviour
 
         return anchor;
     }
+
 }
